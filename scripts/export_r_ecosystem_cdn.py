b3fa4067f9c42aa887660d4d3acff333db52c31e
praster1 <praster1@gmail.com>
2026-05-28 17:22:24 +0900

    Exclude Posit IRL from R Weekly expansion

--- scripts/export_r_ecosystem_cdn.py
+++ scripts/export_r_ecosystem_cdn.py
@@ -446,6 +446,7 @@ SELECT external_id,
            OR positionCaseInsensitive(canonical_url, 'jumpingrivers.github.io/meetingsR/events.html') > 0
            OR positionCaseInsensitive(canonical_url, 'developer.r-project.org/blosxom.cgi/R-devel/NEWS') > 0
            OR positionCaseInsensitive(canonical_url, 'serve.podhome.fm/r-weekly-highlights') > 0
+           OR positionCaseInsensitive(canonical_url, 'forum.posit.co/c/irl') > 0
            OR (
                positionCaseInsensitive(canonical_url, 'dirk.eddelbuettel.com/blog/') > 0
                AND position(canonical_url, '#') > 0
